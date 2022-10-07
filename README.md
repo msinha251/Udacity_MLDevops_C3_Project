ce1350458e41ef964b53f4313974803e655dd842
Mahesh Sinha <mahesh.sinha@workday.com>
2022-10-07 17:54:42 +0200

    Added more test cases and readme updated

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 This repo contains the project files for Udacity MlDevops NanoDegree Course-3 `Deploying a Machine Learning Model on Heroku with FastAPI`
 
-Created pipenv files for environemnt libs.
+# Project Overview:
+In this project, you will apply the skills you have acquired in this course to develop a classification model on publicly available Census Bureau data. You will create unit tests to monitor the model performance on various slices of the data. Then, you will deploy your model using the FastAPI package and create API tests. Both the slice-validation and the API tests will be incorporated into a CI/CD framework using GitHub Actions.
 
-`pipenv install pandas scikit-learn numpy`
+Two datasets will be provided in the starter code on the following page to experience updating the dataset and model in git.
+
+The project contains:
+
+##### CI with GitHub Actions
+* Set up git with GitHub Actions for CI
+
+##### Model building
+* Create a machine learning model.
+* Write unit tests.
+* Write a function that computes model metrics on slices of the data.
+* Write a model card.
+
+##### API Creation
+* Create a REST API using Flask API
+* Create tests for an API.
+
+##### API Deployment
+* Deploy an app to Heroku.
+* Query live API.
+
+
+
+
+
--- tests/test_build_model.py
+++ tests/test_build_model.py
@@ -1,5 +1,5 @@
 import pytest
-from build_model import load_data, basic_cleaning, train_model, save_model, predict
+from build_model import load_data, basic_cleaning, train_model, save_model, save_model_card, predict_batch, predict_single, load_model
 import os
 
 
@@ -40,3 +40,40 @@ def test_save_model(data):
     assert os.path.exists('./model/dv.bin')
     assert os.path.exists('./model/model1.bin')
     assert True
+
+def test_save_model_card(data):
+    save_model_card('./model')
+    assert os.path.exists('./model/model_card.md')
+
+def test_predict_batch(data):
+    df_train, df_test = data
+    df_train, cols = basic_cleaning(df_train)
+    dv, model = train_model(df_train, cols)
+    y_pred = predict_batch(df_test, dv, model, cols)
+    assert y_pred.shape[0] == df_test.shape[0]
+    assert y_pred.min() >= 0
+    assert y_pred.max() <= 1
+
+def test_predict_single(data):
+    sample_dict = {     
+        'workclass': 'state_gov',
+        'education': 'bachelors',
+        'marital_status': 'never_married',
+        'occupation': 'adm_clerical',
+        'relationship': 'not_in_family',
+        'race': 'white',
+        'sex': 'male',
+        'native_country': 'united_states',
+        'age': 39,
+        'fnlwgt': 77516,
+        'education_num': 13,
+        'capital_gain': 2174,
+        'capital_loss': 0,
+        'hours_per_week': 40
+    }
+    df_train, _ = data
+    df_train, cols = basic_cleaning(df_train)
+    dv, model = train_model(df_train, cols)
+    y_pred = predict_single(sample_dict, dv, model)
+    assert y_pred >= 0
+    assert y_pred <= 1
