ac353fad8c012c59ee1ae015d31615333fbab59f
Mahesh Sinha <mahesh.sinha@workday.com>
2022-10-10 13:18:27 +0200

    adding test data for test cases

--- tests/test_clean_data.py
+++ tests/test_clean_data.py
@@ -6,35 +6,35 @@ import pandas as pd
 
 @pytest.fixture(scope="session")
 def load_data():
-    data = pd.read_csv("data/census.csv")
+    data = pd.read_csv("./tests/test_data/census.csv")
     return data
 
 def test_load_data(load_data):
-    assert load_data.shape == (32561, 15)
+    assert load_data.shape == (99, 15)
 
 def test_cleaning(load_data):
-    cleaned_data, cat_cols, num_cols = basic_cleaning(load_data, "data/census_cleaned.csv", "salary")
-    assert cleaned_data.shape == (32561, 15)
+    cleaned_data, cat_cols, num_cols = basic_cleaning(load_data, "./tests/test_data/census_cleaned.csv", "salary")
+    assert cleaned_data.shape == (99, 15)
     assert cleaned_data.isna().sum().sum() == 0
     assert len(cat_cols) == 8
     assert len(num_cols) == 6
 
 def test_process_data(load_data):
-    cleaned_data, cat_cols, num_cols = basic_cleaning(load_data, "data/census_cleaned.csv", "salary", test=True)
+    cleaned_data, cat_cols, num_cols = basic_cleaning(load_data, "./tests/test_data/census_cleaned.csv", "salary", test=True)
     X_train, y_train, encoder, lb = process_data(cleaned_data, categorical_features=cat_cols, label="salary", training=True)
-    assert X_train.shape == (32561, 105)
-    assert y_train.shape == (32561,)
+    assert X_train.shape == (99, 65)
+    assert y_train.shape == (99,)
     assert encoder is not None
     assert lb is not None
 
 def test_train_model(load_data):
-    cleaned_data, cat_cols, num_cols = basic_cleaning(load_data, "data/census_cleaned.csv", "salary", test=True)
+    cleaned_data, cat_cols, num_cols = basic_cleaning(load_data, "./tests/test_data/census_cleaned.csv", "salary", test=True)
     X_train, y_train, encoder, lb = process_data(cleaned_data, categorical_features=cat_cols, label="salary", training=True)
     model = train_model(X_train, y_train)
     assert model is not None
 
 def test_compute_model_metrics(load_data):
-    cleaned_data, cat_cols, num_cols = basic_cleaning(load_data, "data/census_cleaned.csv", "salary", test=True)
+    cleaned_data, cat_cols, num_cols = basic_cleaning(load_data, "./tests/test_data/census_cleaned.csv", "salary", test=True)
     X_train, y_train, encoder, lb = process_data(cleaned_data, categorical_features=cat_cols, label="salary", training=True)
     model = train_model(X_train, y_train)
     X_test, y_test, _, _ = process_data(cleaned_data, categorical_features=cat_cols, label="salary", training=False, encoder=encoder, lb=lb)
@@ -45,7 +45,7 @@ def test_compute_model_metrics(load_data):
     assert fbeta is not None
 
 def test_inference(load_data):
-    cleaned_data, cat_cols, num_cols = basic_cleaning(load_data, "data/census_cleaned.csv", "salary", test=True)
+    cleaned_data, cat_cols, num_cols = basic_cleaning(load_data, "./tests/test_data/census_cleaned.csv", "salary", test=True)
     X_train, y_train, encoder, lb = process_data(cleaned_data, categorical_features=cat_cols, label="salary", training=True)
     model = train_model(X_train, y_train)
     X_test, y_test, _, _ = process_data(cleaned_data, categorical_features=cat_cols, label="salary", training=False, encoder=encoder, lb=lb)
